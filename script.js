dd66c9b162bf0e90df6f165001364fdf4f484287
jackyzha0 <j.zhao2k19@gmail.com>
2019-12-16 15:24:44 -0500

    wip project cards

--- script.js
+++ script.js
@@ -331,3 +331,12 @@ $(document).keydown(function(e) {
   }
   e.preventDefault(); // prevent the default action (scroll / move caret)
 });
+
+// Project Accordion
+$(function() {
+  $(".accordion > .accordion-item.is-active .proj_left").children(".accordion-panel").slideDown();
+
+  $(".accordion > .accordion-item > .proj_left").click(function() {
+    $(this).toggleClass("is-active").children(".accordion-panel").slideToggle("ease-out");
+  });
+});
