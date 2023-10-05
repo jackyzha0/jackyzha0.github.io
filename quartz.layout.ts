503a19199d3978d3d5e0337071a77660942af2bd
Jacky Zhao <j.zhao2k19@gmail.com>
2023-10-05 13:33:34 -0700

    remove breadcrumbs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,7 +17,6 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
